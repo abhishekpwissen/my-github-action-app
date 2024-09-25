f70cf9ebb1e6a4d92570cbf1b018fc1992c2756c
abhishek pandey <abhishekkumar.pandey@wissen.com>
2024-09-25 12:25:54 +0530

    add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+    },
     {
         path: "home",
         component: HomeComponent,
     },
-       {
+    {
         path: "about",
         component: AboutComponent,
+    },
+    {
+        path: "**",
+        component: NotFoundComponent,
     }
 ];
